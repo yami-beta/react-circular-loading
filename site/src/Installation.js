869b3e77cd864766089a11beb56fca4afb4fce37
yami_beta <yami-beta@users.noreply.github.com>
2018-03-23 11:29:43 +0900

    Update site

--- site/src/Installation.js
+++ site/src/Installation.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import SyntaxHighlight from './SyntaxHighlight';
+import React from "react";
+import SyntaxHighlight from "./SyntaxHighlight";
 
-const code = `$ npm install @yami_beta/react-circular-loading
+const code = `$ npm install styled-components @yami_beta/react-circular-loading
 $ # OR
-$ yarn add @yami_beta/react-circular-loading`;
+$ yarn add styled-components @yami_beta/react-circular-loading`;
 
-const Installation = (props) => {
+const Installation = props => {
   return (
     <section className="installation">
       <h2>Installation</h2>
-      <SyntaxHighlight code={ code } lang="bash" />
+      <SyntaxHighlight code={code} lang="bash" />
     </section>
   );
 };
